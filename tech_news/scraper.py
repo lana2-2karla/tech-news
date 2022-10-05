0091de183af31523d23b06c0cc9861fa448e6922
lana2-2karla <allanakarla1702@gmail.com>
2022-10-05 17:36:11 -0300

    :sparkles: feat: função scrape_noticia

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -22,9 +22,7 @@ def scrape_novidades(html_content):
         return []
     else:
         selector = Selector(text=html_content)
-        links = selector.css(
-            ".entry-thumbnail a::attr(href)"
-        ).getall()
+        links = selector.css(".entry-thumbnail a::attr(href)").getall()
         return links
 
 
@@ -33,9 +31,21 @@ def scrape_next_page_link(html_content):
     return selector.css(".next::attr(href)").get()
 
 
-# Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    info_news = {
+        "url": selector.css("link[rel=canonical]::attr(href)").get(),
+        "title": selector.css("h1.entry-title::text").get().strip(),
+        "timestamp": selector.css("li.meta-date::text").get(),
+        "writer": selector.css(".author ::text").get(),
+        "comments_count": len(selector.css("ol.comment-list").getall()) or 0,
+        "summary": "".join(
+            selector.css(".entry-content > p:nth-of-type(1) ::text").getall()
+        ).strip(),
+        "tags": selector.css("a[rel=tag]::text").getall(),
+        "category": selector.css("span.label::text").get(),
+    }
+    return info_news
 
 
 # Requisito 5
