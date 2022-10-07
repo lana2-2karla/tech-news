83fddd39531e9d3d5c96efa3b03457554e639fed
lana2-2karla <allanakarla1702@gmail.com>
2022-10-07 19:31:27 -0300

    :sparkles: feat: função search_by_title

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,9 @@
-# Requisito 6
+from tech_news.database import search_news
+
+
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news_list = search_news({"title": {"$regex": title, "$options": "i"}})
+    return [(news["title"], news["url"]) for news in news_list]
 
 
 # Requisito 7
