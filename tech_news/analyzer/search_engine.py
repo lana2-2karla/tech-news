fe1d367612eb9179db9dbd00cec86f5ba866ed8a
lana2-2karla <allanakarla1702@gmail.com>
2022-10-07 23:15:58 -0300

    :bug: fix: search_by_date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -6,9 +6,16 @@ def search_by_title(title):
     return [(news["title"], news["url"]) for news in news_list]
 
 
-# Requisito 7
-def search_by_date(date):
-    """Seu código deve vir aqui"""
+def search_by_date(date_iso):
+    try:
+        date = date_iso.split("-")
+        date_formated = f"{date[2]}/{date[1]}/{date[0]}"
+
+        news_list = search_news({"timestamp": date_formated})
+        return [(news["title"], news["url"]) for news in news_list]
+
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
