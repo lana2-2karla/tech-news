27abbeb8df942722460cc9b2ebe54f7981100a4a
lana2-2karla <allanakarla1702@gmail.com>
2022-10-08 10:23:25 -0300

    :sparkles: feat: função search_by_tag

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -18,9 +18,9 @@ def search_by_date(date_iso):
         raise ValueError("Data inválida")
 
 
-# Requisito 8
 def search_by_tag(tag):
-    """Seu código deve vir aqui"""
+    news_list = search_news({"tags": {"$regex": tag, "$options": "i"}})
+    return [(news["title"], news["url"]) for news in news_list]
 
 
 # Requisito 9
